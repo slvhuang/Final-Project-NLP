670ca77212138941a85aaf3dfedcfaf5c82f02de
Silvia <status>
2023-05-21 20:11:41 -0500

    targeted sentiment ner

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import spacy
 import nltk
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
 
 nlp = spacy.load('en_core_web_lg')
 
@@ -14,4 +17,21 @@ def process_text_sen(text):
     for sent in sents:
         doc = nlp(sent)
         ents.extend([(ent.text, ent.label_) for ent in doc.ents])
+    return ents
+
+
+def process_text_sen_sentiment(text):
+    sents = nltk.sent_tokenize(text)
+    ents = []
+    for sent in sents:
+
+        # add sentiment
+        blob = TextBlob(sent)
+        polarity = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
+
+        # ner
+        doc = nlp(sent)
+        ents.extend([(ent.text, ent.label_, polarity, subjectivity) for ent in doc.ents])
+
     return ents
